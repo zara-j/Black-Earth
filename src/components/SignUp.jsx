ec2739f3de5152ebc6fa9d1f42bbfffde616834e
javadi <javadi89s@gmail.com>
2024-08-30 13:22:03 +0330

    Update Project

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -55,11 +55,11 @@ export default function SignUp() {
                   <div className="md:mx-6 md:p-12">
                     {/* <!--Logo--> */}
                     <div className="text-center">
-                      <img
+                      {/* <img
                         className="mx-auto w-48"
-                        src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                        alt="logo"
-                      />
+                        src="./src/assets/bg1.jpg"
+                      // alt="logo"
+                      /> */}
                       <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
                         Black Earth
                       </h4>
@@ -130,7 +130,7 @@ export default function SignUp() {
                         <div rippleColor="light" className="w-full">
                           <button
                             className="mb-3 inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                            type="button"
+                            type="submit"
                             style={{
                               background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
                             }}
@@ -142,44 +142,28 @@ export default function SignUp() {
                         {/* <!--Forgot password link--> */}
                         <a href="#!">Terms and conditions</a>
                       </div>
-
-                      {/* <!--Register button--> */}
-                      <div className="flex items-center justify-between pb-6">
-                        <p className="mb-0 mr-2">Have an account?</p>
-                        <div>
-                          <button style={{
-                            background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
-                          }}
-                          className="mb-3 inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"                          >
-                            Sign In
-                          </button>
-
-
-                        </div>
-                      </div>
                     </form>
+
+                    <div className='flex items-center justify-center align-text-bottom'>
+                      <div className="flex items-center justify-center pb-6">
+                        <p className="mb-0 mr-2 mt-3">Have an account?</p>
+                      </div>
+                      <button
+                        onClick={() => navigate('/signin')}
+                        style={{
+                          background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                        }}
+                        className="mb-3 inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"                          >
+                        Sign In
+                      </button>
+
+                    </div>
                   </div>
                 </div>
 
                 {/* <!-- Right column container with background and description--> */}
-                <div
-                  className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
-                  style={{
-                    background:
-                      "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
-                  }}
-                >
-                  <div className="px-4 py-6 text-white md:mx-6 md:p-12">
-                    <h4 className="mb-6 text-xl font-semibold">
-                      We are more than just a company
-                    </h4>
-                    <p className="text-sm">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <img src="./src/assets/bg1.jpg" alt="black-earth" className='h-full' />
                 </div>
               </div>
             </div>
